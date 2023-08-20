cb8f39f4c965d8bfbe2b4dba8b70d4c3187b07d2
egepancaroglu <egepancaroglu@gmail.com>
2023-08-21 00:14:01 +0300

    Dev

--- read-stream-app/src/main/java/com/springkafkaproductrecommendation/service/EventService.java
+++ read-stream-app/src/main/java/com/springkafkaproductrecommendation/service/EventService.java
@@ -1,11 +1,22 @@
 package com.springkafkaproductrecommendation.service;
 
 import com.springkafkaproductrecommendation.entity.Event;
+import com.springkafkaproductrecommendation.entity.ProductProperties;
 import com.springkafkaproductrecommendation.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Service
 public class EventService {
+
+
+    private MongoTemplate mongoTemplate;
 
     private final EventRepository eventRepository;
 
@@ -17,4 +28,31 @@ public class EventService {
         eventRepository.save(event);
     }
 
+    public List<Event> getEvents() {
+        return eventRepository.findAll();
+    }
+
+    public List<Event> getLast10ProductViewsByUserId(String userId) {
+        return eventRepository.findByUseridAndEventOrderByTimestampDesc(userId, "ProductView", PageRequest.of(0, 10));
+    }
+
+    public Set<String> getProducts() {
+        List<Event> events = eventRepository.findAll();
+        Set<String> productIds = new HashSet<>();
+
+        for (Event event : events) {
+            ProductProperties properties = event.getProperties();
+            if (properties != null) {
+                String productId = properties.getProductid();
+                if (productId != null) {
+                    productIds.add(productId);
+                }
+            }
+        }
+
+        return productIds;
+    }
+
 }
+
+
